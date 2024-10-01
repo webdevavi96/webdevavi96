98b376598c95451ae6a681bdf6fa352de57a1042
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-01 10:46:06 +0530

    Add files via upload

---
+++ equal.php
@@ -0,0 +1,10 @@
+<?php
+$a = 7264166976;
+$b =4636737336437;
+if ($a ==$b) {
+	echo "$a and $b are equal";
+}
+else {
+	echo "$a and $b is not equal";
+}
+?>
---
+++ equal1.php
@@ -0,0 +1,12 @@
+<?php
+echo "Enter two numbers to check they ae equal or not => \n";
+$a = readline();
+$b = readline();
+echo "You entered these numbers: $a and $b\n";
+if ($a == $b) {
+echo "Sab changa hai ji  😎 kyoki $a aur $b equal hai";
+}
+else {
+	echo "dekh bhai $a ki aukat $b jitani nahi hai\n";
+}
+?>
