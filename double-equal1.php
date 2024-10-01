018e545fe2c0a132e2d99decc192a1137c66c765
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-01 11:26:57 +0530

    update double-equal1.php

--- double-equal1.php
+++ double-equal1.php
@@ -1,11 +1,10 @@
 <?php
 $a = 351617;
 $b = 'dh';
-echo "$a and $b";
+echo "$a and $b \n";
 if ($a === $b) {
 		var_dump($a === $b);
 	echo "Data type and Value both or equal in $a and $b";
-	echo PHP_EOL;
 }
 else {
 	echo "value or data type is different \n";
