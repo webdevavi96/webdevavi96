702a04d2797c665960d94c6356d570f7651efa30
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-01 11:25:32 +0530

    update double-equal1.php

--- double-equal1.php
+++ double-equal1.php
@@ -3,8 +3,8 @@ $a = 351617;
 $b = 'dh';
 echo "$a and $b";
 if ($a === $b) {
+		var_dump($a === $b);
 	echo "Data type and Value both or equal in $a and $b \n";
-	var_dump($a === $b);
 }
 else {
 	echo "value or data type is different \n";
