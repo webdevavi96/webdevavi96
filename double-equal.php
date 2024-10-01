389493d52956721df66117b667834c29137209b4
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-01 11:08:20 +0530

    update double-equal.php

--- double-equal.php
+++ double-equal.php
@@ -1,3 +1,15 @@
 <?php
-echo"hello world";
+echo "Enter any number or string: \n";
+$a = readline();
+echo PHP_EOL;
+echo "Enter second number or string: \n";
+$b = readline();
+if ($a === $b) {
+	echo "Data type and Value both or equal\n";
+	var_dump($a === $b);
+}
+else {
+	echo "value or dqta type is different\n";
+	var_dump($a === $b);
+}
 ?>
