48f9f971513359d2d396ec30c2aadf3097f0c7aa
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-02 16:24:20 +0530

    update less1.php

--- myphp/less1.php
+++ myphp/less1.php
@@ -2,11 +2,11 @@
 $num1 = 0.4577;
 $num2 = 0.8751;
 echo "between $num1 and $num2 which is less than other?\n";
-if ($num1 > $num2) {
-	echo "$num1 is greater than $num2";
+if ($num1 < $num2) {
+	echo "$num1 is less than $num2";
 }
 else {
-	echo "$num2 is greater than $num1";
+	echo "$num2 is less than $num1";
 }
 echo PHP_EOL;
 ?>
