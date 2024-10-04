57ae9875f85b784a318d6786b02f14f9ddd07d44
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-04 12:47:56 +0530

    update

---
+++ myphp2/and operator/and4.php
@@ -0,0 +1,13 @@
+<?php
+$a = 56;
+$b = 'hello';
+echo "$a and $b \n";
+if ($a === 57 && $b === 'dh') {
+		var_dump($a === 57 && $b === 'bye');
+	echo "Everything looks good! \n";
+}
+else {
+	echo "Kucch to gadbad hai !!! \n";
+	var_dump($a === 56 && $b === 'hello');
+}
+?>
---
+++ myphp2/and operator/and5.php
@@ -0,0 +1,13 @@
+<?php
+$a = 56;
+$b = 46;
+echo "$a and $b \n";
+if ($a != 99 && $b != 50) {
+		var_dump($a != 99 && $b != 50);
+	echo "Everything looks good! \n";
+}
+else {
+	echo "Kucch to gadbad hai !!! \n";
+	var_dump($a == 99 && $b == 50);
+}
+?>
