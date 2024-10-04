3b6f053648fcfde2c3a017a82b164129bd368187
webdevavi96 <avinashchaurasiya902@gmail.com>
2024-10-04 13:43:36 +0530

    update

---
+++ myphp2/not operator/not4.php
@@ -0,0 +1,12 @@
+<?php
+$a = 351617;
+echo "$a\n";
+if (!$a !== '351617') {
+		var_dump(!$a !== '351617');
+	echo "Everything looks good! \n";
+}
+else {
+	echo "Kucch to gadbad hai !!! \n kyoki";
+	var_dump($a == 351617);
+}
+?>
---
+++ myphp2/not operator/not7.php
@@ -0,0 +1,12 @@
+<?php
+$a = 66;
+echo "$a <= 99 \n";
+if (!$a <= '351617') {
+		var_dump(!$a <= 99);
+	echo "Everything looks good! \n";
+}
+else {
+	echo "Kucch to gadbad hai !!! \n kyoki";
+	var_dump($a >= 66);
+}
+?>
